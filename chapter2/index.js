a87d6e87571b7fbc04285122251963479832d1f8
wagner <wggauer@gmail.com>
2023-06-29 21:31:52 -0300

    add oficial solutions

--- chapter2/index.js
+++ chapter2/index.js
@@ -1,9 +1,13 @@
 // First
+// Mine
 for (let string = ""; string.length <= 7; string += "#") {
   console.log(string);
 }
+// Solution
+for (let line = "#"; line.length < 8; line += "#") console.log(line);
 
-// Second
+// Second ==============================================
+// Mine
 for (let count = 1; count <= 100; count++) {
   if (count % 3 === 0) {
     console.log("Fizz");
@@ -14,8 +18,16 @@ for (let count = 1; count <= 100; count++) {
   }
 }
 
-// Third
+// Solution
+for (let n = 1; n <= 100; n++) {
+  let output = "";
+  if (n % 3 == 0) output += "Fizz";
+  if (n % 5 == 0) output += "Buzz";
+  console.log(output || n);
+}
 
+// Third =============================================
+// Mine
 const generateGrid = (size) => {
   let grid = "";
 
@@ -51,3 +63,21 @@ const generateGrid = (size) => {
 };
 
 generateGrid(8);
+
+// Solution:
+let size = 8;
+
+let board = "";
+
+for (let y = 0; y < size; y++) {
+  for (let x = 0; x < size; x++) {
+    if ((x + y) % 2 == 0) {
+      board += " ";
+    } else {
+      board += "#";
+    }
+  }
+  board += "\n";
+}
+
+console.log(board);
