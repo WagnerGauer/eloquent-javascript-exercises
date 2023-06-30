2a2be90fc367f3cab389a7b64d1711d201aef768
wagner <wggauer@gmail.com>
2023-06-30 02:41:48 -0300

    add code related to the content in chapter 4

---
+++ chapter4/index.js
@@ -0,0 +1,61 @@
+import { JOURNAL } from "./journal.js";
+
+console.log(5 * 85 * 10 * 80);
+console.log(40 / Math.sqrt(5 * 85 * 10 * 80));
+
+function phi(table) {
+  return (
+    (table[3] * table[0] - table[2] * table[1]) /
+    Math.sqrt(
+      (table[2] + table[3]) *
+        (table[0] + table[1]) *
+        (table[1] + table[3]) *
+        (table[0] + table[2])
+    )
+  );
+}
+
+function tableFor(event, journal) {
+  let table = [0, 0, 0, 0];
+  for (let i = 0; i < journal.length; i++) {
+    let entry = journal[i],
+      index = 0;
+    if (entry.events.includes(event)) index += 1;
+    if (entry.squirrel) index += 2;
+    table[index] += 1;
+  }
+  return table;
+}
+
+console.log(tableFor("pizza", JOURNAL));
+
+const journalEvents = (journal) => {
+  const events = [];
+
+  for (let entry of journal) {
+    for (let event of entry.events) {
+      if (!events.includes(event)) {
+        events.push(event);
+      }
+    }
+  }
+  return events;
+};
+
+for (let event of journalEvents(JOURNAL)) {
+  let correlation = phi(tableFor(event, JOURNAL));
+  if (correlation > 0.1 || correlation < -0.1) {
+    console.log(event + ":", correlation);
+  }
+}
+
+for (let entry of JOURNAL) {
+  if (
+    entry.events.includes("peanuts") &&
+    !entry.events.includes("brushed teeth")
+  ) {
+    entry.events.push("peanut teeth");
+  }
+}
+
+console.log(phi(tableFor("peanut teeth", JOURNAL)));
